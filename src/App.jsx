1871877b9e819a053330ce2efb29c422f8d89ad4
tootsie60 <73935697+tootsie60@users.noreply.github.com>
2022-08-02 00:35:54 -0700

    initial commit by Kevin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardMedia, CardContent, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import PhotoCamera from '@mui/icons-material';
+    
+const App = () => {
+    return (
+        <div className="">
+            <Typography variant="h1">Hello World!</Typography>
+        </div>
+    )
+}
+
+export default App;
